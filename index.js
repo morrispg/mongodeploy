def63c40c8fe7371a66771c1dfcb504c43a31a96
morrispg <124636880+morrispg@users.noreply.github.com>
2023-05-22 12:55:20 -0500

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(express.static("public"));
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('01-Activities')
   ? cwd.split('/01-Activities/')[1]
